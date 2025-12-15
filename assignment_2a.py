23759879443587df76f12d1fca5f4ec359284abb
yashkolage20 <yash.kolage@mitwpu.edu.in>
2025-12-15 11:45:05 +0530

    Add files via upload

---
+++ assignment_2a.py
@@ -0,0 +1,5 @@
+a = int(input("enter a number: ")) #takes input from the user
+if a % 2  == 0: #checks if the number is even or odd
+    print(f" {a} is an Even Number")
+else:
+    print(f"{a} is an Odd Number7")
---
+++ assignment_2b.py
@@ -0,0 +1,4 @@
+sum = 0
+for i in range(0, 51):
+    sum += i
+print(f"Sum of numbers from 1 to 50 is: {sum}")
